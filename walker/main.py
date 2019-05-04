ce0c59f7fee97366250d1d6a83d8a28f2b31be36
lebenlechzer <lebenlechzer@gmail.com>
2019-05-04 13:37:08 +0200

    First pygame loop

--- walker/main.py
+++ walker/main.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf8 :
 
-def main(argv):
-    pass
+import sys
+import pygame
+import pygame.locals
+import pymunk
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Walker")
+
+    screen = pygame.display.set_mode((240,180))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or \
+              (event.type == pygame.locals.KEYDOWN and
+               event.key == pygame.locals.K_ESCAPE):
+                running = False
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    sys.exit(main())
